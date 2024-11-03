dec485bb8a2ec88cca6408848695af624c3d8689
Peyjee-W <peyjeewang@gmail.com>
2024-11-03 21:10:06 +0800

    e成

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
 // 调试信息
 console.log('Attempting to connect to MongoDB...');
 
@@ -19,15 +17,14 @@ console.log('Attempting to connect to MongoDB...');
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('MongoDB connected');
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
     })
     .catch(err => {
         console.error('MongoDB connection error:', err);
     });
 
-    const transactionsRouter = require('./routes/transactions');  // 引入交易路由
-    app.use('/transactions', transactionsRouter);                 // 将路由添加到应用程序中
-    
-// 导出 Express 应用程序（Vercel 使用）module.exports = app; 
+// 引入交易路由，并将路由前缀设为 `/api/transactions`
+const transactionsRouter = require('./routes/transactions');
+app.use('/api/transactions', transactionsRouter);
+
+// 导出 Express 应用（Vercel 使用）
+module.exports = app;
